48ad2ac0094e8fae638deca424551abc0bd1474c
bluesauce838 <94112413+bluesauce838@users.noreply.github.com>
2022-11-17 20:19:09 +0530

    1 file modified, 1 added

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,46 +1,17 @@
 import React, { useState } from 'react';
-import swal from 'sweetalert2';
-// function Add ()=>{
-//   const[firstName,seFirstname]=useState('')
-//   const handleAdd = e =>{
-//   e.preventDefault();
-//   if (!firstName)
-//   return(
-//     'dont call'
-//   )
-// }
-// return (
-//   <div className='small-container'>
-//     <form onSubmit={ handleAdd }>
-//       <h1>
-//         Add Employee
-//         </h1>
-//         <label htmlFor="firstNAme">FirstName </label>
-//         <Input
-//         id="lastName"
-//         type = "text"
-//         name="FirstName"
-//         value={firstName}
-//         onChange={e=>setFirstName(e.target.value)}
-//         />
-//         </form>
-//         </div>
-// )
-// };
+
 
 function Add() {
   return (
     <div>
       <form>
-      
-  <label>
-    Name:
-    <input type="text" name="name" />
-  </label>
-  <input type="submit" value="Submit" />
-
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
       </form>
-      {/* x {props.name} */}
+ 
     </div>
   );
 }
